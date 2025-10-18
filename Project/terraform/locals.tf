85dabbd21854ade172000bdcc87368d2c5e3a457
Gie.kaano <66070091@kmitl.ac.th>
2025-10-18 12:55:39 +0700

    modified: change all variable on module.tf to local.tf

--- Project/terraform/locals.tf
+++ Project/terraform/locals.tf
@@ -1,9 +1,183 @@
 
+# Common Defined Prefixes and Variables
 locals {
+  # Common prefixes for resource names and tags
   tags_prefix = "itkmitl"
   name_prefix = "prompt"
 
   default_name = "${local.name_prefix}91"
 
+  # AMI for EC2 instances
   ami = "ami-052064a798f08f0d3"
+}
+
+# Tags Defined
+locals {
+  # VPC tags variables
+  vpc_tags = "${local.default_name}-VPC"
+  igw_tags = "${local.default_name}-testIGW"
+
+  # Security Group tags variables
+  lb_sg_tags       = "${local.default_name}-lb-SG"
+  instance_sg_tags = "${local.default_name}-instance-SG"
+
+  # Instance tags variable
+  instance_tags = "${local.default_name}-servers"
+
+  # Load Balancer tags variable
+  lb_tags           = "${local.default_name}-web-loadbalancer"
+  target_group_tags = "${local.default_name}-tgp"
+  listener_tags     = "${local.default_name}-listener"
+}
+
+# Networking Variables Defined
+locals {
+  # VPC variable
+  network_address_space = "10.0.0.0/16"
+  enable_dns_hostnames  = true
+
+  # subnet variable
+  public_subnet_count  = 2
+  private_subnet_count = 2
+
+  ## [public subnet, public subnet, private subnet]
+  subnet_address_space = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones   = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  ## [public subnets, private subnet]
+  map_public_ip_on_launch = [true, false]
+
+  # route table variable
+  rt_cidr_block = "0.0.0.0/0"
+
+  public_subnet_tags = [
+    "${local.default_name}-publicsubnet1",
+    "${local.default_name}-publicsubnet2"
+  ]
+  private_subnet_tags = [
+    "${local.default_name}-privatesubnet1",
+    "${local.default_name}-privatesubnet2"
+  ]
+
+  public_route_table_tags  = "${local.default_name}-publicRouteTable"
+  private_route_table_tags = "${local.default_name}-privateRouteTable"
+}
+
+# Security Group Variables Defined
+locals {
+  # Instance Security Group variables
+  instance_sg_name        = "${local.default_name}_ssh_web_sg"
+  instance_sg_description = "Allow SSH and HTTP access"
+
+  instance_ingress_rules = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  instance_egress_rules = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  # LB Security Group variables
+  lb_sg_name        = "${local.default_name}-lb-SG"
+  lb_sg_description = "Allow HTTP and HTTPS access"
+
+  lb_ingress_rules = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+
+  lb_egress_rules = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+# Instance Variables Defined
+locals {
+
+  instance_count = 2
+
+  instance_type     = "t2.micro"
+  instance_key_name = "vockey"
+}
+
+# Load Balancer Variables Defined
+locals {
+
+  lb_name     = "server-loadbalancer"
+  lb_internal = false
+  lb_type     = "application"
+
+  lb_deletion_protection = false
+
+  # target group variable
+  tg_name     = "server-tg"
+  tg_port     = 80
+  tg_protocol = "HTTP"
+
+  ## health check variable
+  hc_enabled   = true
+  hc_healthy   = 2
+  hc_unhealthy = 2
+  hc_timeout   = 5
+  hc_protocol  = "HTTP"
+  hc_path      = "/"
+  hc_interval  = 30
+  hc_matcher   = "200"
+
+  # listener variable
+  listener_port     = 80
+  listener_protocol = "HTTP"
+  ## default_action
+  default_action_type = "forward"
+}
+
+# Database Variables Defined
+locals {
+  # Database subnet group variable
+  subnet_group_name = "${local.default_name}-db-subnet-group"
+
+  # Database variable
+  allocated_storage = 10
+  db_name           = "testdb"
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  identifier        = "${local.default_name}-db-instance"
+
+  username = "admin"
+  password = "password"
+
+  group_name          = "default.mysql8.0"
+  skip_final_snapshot = true
+  availability_zone   = "us-east-1c"
 }
--- Project/terraform/module.tf
+++ Project/terraform/module.tf
@@ -3,37 +3,31 @@ module "networking" {
   source = "./modules/networking"
 
   # vpc variable
-  network_address_space = "10.0.0.0/16"
-  enable_dns_hostnames  = true
+  network_address_space = local.network_address_space
+  enable_dns_hostnames  = local.enable_dns_hostnames
 
   # subnet variable
-  public_subnet_count  = 2
-  private_subnet_count = 2
+  public_subnet_count  = local.public_subnet_count
+  private_subnet_count = local.private_subnet_count
 
   ## [public subnet, public subnet, private subnet]
-  subnet_address_space = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones   = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+  subnet_address_space = local.subnet_address_space
+  availability_zones   = local.availability_zones
   ## [public subnets, private subnet]
-  map_public_ip_on_launch = [true, false]
+  map_public_ip_on_launch = local.map_public_ip_on_launch
 
   # route table variable
-  rt_cidr_block = "0.0.0.0/0"
+  rt_cidr_block = local.rt_cidr_block
 
   # tags variable
-  vpc_tags = "${local.default_name}-VPC"
-  igw_tags = "${local.default_name}-testIGW"
+  vpc_tags = local.vpc_tags
+  igw_tags = local.igw_tags
 
-  public_subnet_tags = [
-    "${local.default_name}-publicsubnet1",
-    "${local.default_name}-publicsubnet2"
-  ]
-  private_subnet_tags = [
-    "${local.default_name}-privatesubnet1",
-    "${local.default_name}-privatesubnet2"
-  ]
+  public_subnet_tags  = local.public_subnet_tags
+  private_subnet_tags = local.private_subnet_tags
 
-  public_route_table_tags  = "${local.default_name}-publicRouteTable"
-  private_route_table_tags = "${local.default_name}-privateRouteTable"
+  public_route_table_tags  = local.public_route_table_tags
+  private_route_table_tags = local.private_route_table_tags
 }
 
 module "security-group" {
@@ -43,80 +37,39 @@ module "security-group" {
   vpc_id = module.networking.vpc_id
 
   # instance security group variable
-  instance_sg_name        = "${local.default_name}_ssh_web_sg"
-  instance_sg_description = "Allow SSH and HTTP access"
+  instance_sg_name        = local.instance_sg_name
+  instance_sg_description = local.instance_sg_description
 
-  instance_ingress_rules = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  instance_ingress_rules = local.instance_ingress_rules
 
-  instance_egress_rules = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  instance_egress_rules = local.instance_egress_rules
 
   # LB security group variable
-  lb_name        = "${local.default_name}-lb-SG"
-  lb_description = "Allow HTTP and HTTPS access"
+  lb_name        = local.lb_sg_name
+  lb_description = local.lb_sg_description
 
-  lb_ingress_rules = [
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  lb_egress_rules = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+  lb_ingress_rules = local.lb_ingress_rules
+  lb_egress_rules  = local.lb_egress_rules
 
   # tags variable
-  lb_sg_tags       = "${local.default_name}-lb-SG"
-  instance_sg_tags = "${local.default_name}-instance-SG"
+  lb_sg_tags       = local.lb_sg_tags
+  instance_sg_tags = local.instance_sg_tags
 }
 
 module "instance" {
   source = "./modules/instance"
 
   # instance variable
-  instance_count = 2
+  instance_count = local.instance_count
 
   ami                    = var.ami
-  type                   = "t2.micro"
-  key_name               = "vockey"
+  type                   = local.instance_type
+  key_name               = local.instance_key_name
   vpc_security_group_ids = [module.security-group.instance_sg_id]
   subnet_ids             = module.networking.public_subnet_ids
 
   # tags variable
-  instance_tags = "${local.default_name}-servers"
+  instance_tags = local.instance_tags
 }
 
 module "load-balancer" {
@@ -127,39 +80,39 @@ module "load-balancer" {
   instance_ids = module.instance.instance_ids
 
   # load balancer variable
-  lb_name            = "server-loadbalancer"
-  lb_internal        = false
-  lb_type            = "application"
+  lb_name            = local.lb_name
+  lb_internal        = local.lb_internal
+  lb_type            = local.lb_type
   lb_security_groups = [module.security-group.lb_sg_id]
   lb_subnets         = module.networking.public_subnet_ids
 
-  lb_deletion_protection = false
+  lb_deletion_protection = local.lb_deletion_protection
 
   # target group variable
-  tg_name     = "server-tg"
-  tg_port     = 80
-  tg_protocol = "HTTP"
+  tg_name     = local.tg_name
+  tg_port     = local.tg_port
+  tg_protocol = local.tg_protocol
 
   ## health check variable
-  hc_enabled   = true
-  hc_healthy   = 2
-  hc_unhealthy = 2
-  hc_timeout   = 5
-  hc_protocol  = "HTTP"
-  hc_path      = "/"
-  hc_interval  = 30
-  hc_matcher   = "200"
+  hc_enabled   = local.hc_enabled
+  hc_healthy   = local.hc_healthy
+  hc_unhealthy = local.hc_unhealthy
+  hc_timeout   = local.hc_timeout
+  hc_protocol  = local.hc_protocol
+  hc_path      = local.hc_path
+  hc_interval  = local.hc_interval
+  hc_matcher   = local.hc_matcher
 
   # listener variable
-  listener_port     = 80
-  listener_protocol = "HTTP"
+  listener_port     = local.listener_port
+  listener_protocol = local.listener_protocol
   ## default_action
-  default_action_type = "forward"
+  default_action_type = local.default_action_type
 
   # tags variable
-  lb_tags           = "${local.default_name}-web-loadbalancer"
-  target_group_tags = "${local.default_name}-tgp"
-  listener_tags     = "${local.default_name}-listener"
+  lb_tags           = local.lb_tags
+  target_group_tags = local.target_group_tags
+  listener_tags     = local.listener_tags
 }
 
 module "database" {
@@ -167,20 +120,20 @@ module "database" {
 
   # Database subnet group variable
   private_subnet_ids = module.networking.private_subnet_ids
-  subnet_group_name  = "${local.default_name}-db-subnet-group"
+  subnet_group_name  = local.subnet_group_name
 
   # Database variable
-  allocated_storage = 10
-  db_name           = "testdb"
-  engine            = "mysql"
-  engine_version    = "8.0"
-  instance_class    = "db.t3.micro"
-  identifier        = "${local.default_name}-db-instance"
+  allocated_storage = local.allocated_storage
+  db_name           = local.db_name
+  engine            = local.engine
+  engine_version    = local.engine_version
+  instance_class    = local.instance_class
+  identifier        = local.identifier
 
-  username = "admin"
-  password = "password"
+  username = local.username
+  password = local.password
 
-  group_name          = "default.mysql8.0"
-  skip_final_snapshot = true
-  availability_zone   = "us-east-1c"
+  group_name          = local.group_name
+  skip_final_snapshot = local.skip_final_snapshot
+  availability_zone   = local.availability_zone
 }
